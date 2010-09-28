addc6b85af1e34f8bf6403393905266dd26d207a
anatoljevich <anatoljevich@gmail.com>
2010-09-28 11:57:00 +0300

    commenting out test students create migration

--- db/migrate/20090115145834_create_test_students.rb
+++ db/migrate/20090115145834_create_test_students.rb
@@ -1,16 +1,16 @@
 class CreateTestStudents < ActiveRecord::Migration
   def self.up
-    group = Group.create({:spec => 'TEST'.to_s, :number => 1})
-    group.students.create({:surname => 'Иванов', :name => 'Иван', :parent_name => 'Иванович', :code => '00000001' })
-    group.students.create({:surname => 'Петров', :name => 'Петр', :parent_name => 'Петрович', :code => '00000002' })
-    group.students.create({:surname => 'Алексеев', :name => 'Алексей', :parent_name => 'Алексеевич', :code => '00000003' })
-    group.students.create({:surname => 'Сергеев', :name => 'Сергей', :parent_name => 'Сергеевич', :code => '00000004' })
-    group.students.create({:surname => 'Дмитриев', :name => 'Дмитрий', :parent_name => 'Дмитриевич', :code => '00000005' })
+#    group = Group.create({:spec => 'TEST'.to_s, :number => 1})
+#    group.students.create({:surname => 'Иванов', :name => 'Иван', :parent_name => 'Иванович', :code => '00000001' })
+#    group.students.create({:surname => 'Петров', :name => 'Петр', :parent_name => 'Петрович', :code => '00000002' })
+#    group.students.create({:surname => 'Алексеев', :name => 'Алексей', :parent_name => 'Алексеевич', :code => '00000003' })
+#    group.students.create({:surname => 'Сергеев', :name => 'Сергей', :parent_name => 'Сергеевич', :code => '00000004' })
+#    group.students.create({:surname => 'Дмитриев', :name => 'Дмитрий', :parent_name => 'Дмитриевич', :code => '00000005' })
  end
 
   def self.down
-    group = Group.find_by_spec_and_number "TEST", 1
-    group.students.clear if group
-    group.destroy
+#    group = Group.find_by_spec_and_number "TEST", 1
+#    group.students.clear if group
+#    group.destroy
   end
 end
