3477675c3f09e4270e4c5c35f792797ce7dab098
anatoljevich <anatoljevich@gmail.com>
2010-10-05 13:20:41 +0300

    fixing root routing

--- config/routes.rb
+++ config/routes.rb
@@ -38,9 +38,9 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
+  map.root :controller => 'user'
   map.connect 'stat/', :controller => 'stat', :action => 'topic'  
 
   map.connect ':controller/:action.:format'
   map.connect ':controller/:action/:id.:format'
-  map.connect ':controller/:action/:id', :controller => 'user'
 end
